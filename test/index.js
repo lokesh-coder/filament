1d6ef09ee59ee51a7eda4ceeb7f0c6b56a123741
PROWARENESSDC\l.rajendran <l.rajendran@prowareness.nl>
2017-07-25 19:17:14 +0530

    test: fixing test

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 describe('Array', function () {
     describe('#indexOf()', function () {
         it('should fail ', function () {
-            assert.equal(2, [1, 2, 3].indexOf(9));
+            assert.equal(1, [1, 2, 3].indexOf(2));
         });
     });
 });

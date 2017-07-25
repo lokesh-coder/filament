2871b250c940136143db776cf613fa66bba3450c
PROWARENESSDC\l.rajendran <l.rajendran@prowareness.nl>
2017-07-25 19:23:39 +0530

    test: should pass

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 describe('Array', function () {
     describe('#indexOf()', function () {
-        it('should fail ', function () {
+        it('should fail and pass ', function () {
             assert.equal(1, [1, 2, 3].indexOf(2));
         });
     });

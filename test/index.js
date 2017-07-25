2f2a3865f6f27f345b3847ac1d9964b67e7950ab
PROWARENESSDC\l.rajendran <l.rajendran@prowareness.nl>
2017-07-25 19:10:57 +0530

    test: new error

--- test/index.js
+++ test/index.js
@@ -1,7 +1,7 @@
 var assert = require('assert');
 describe('Array', function () {
     describe('#indexOf()', function () {
-        it('should return -1 when the value is not present ', function () {
+        it('should fail ', function () {
             assert.equal(2, [1, 2, 3].indexOf(9));
         });
     });

73966d3df2cdab7913273fe06da434df520ab9a2
Lokesh <mexican.dirtyfellow@gmail.com>
2017-07-25 19:30:19 +0530

    test: foobar
    * feat: name is changed
    
    * feat: break this
    
    * fix: travis deploy update
    
    * feat( Core ): Deploy npm package via travis
    fix( Core ): did nothing!
    
    * fix: minor travis
    
    * fix: travis pull condition added
    
    * BREAKKING CHANGE: fixes
    
    * fix: travis condition fix
    
    * fix: travis syntax fix
    
    * fix: another travis
    
    * fix: another
    
    * fix: npm
    
    * fix: test
    
    * test: error
    
    * test: set error
    
    * test: new error
    
    * test: fixing test
    
    * test: should pass
    
    * build: travis update

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 var assert = require('assert');
 describe('Array', function () {
     describe('#indexOf()', function () {
-        it('should fail ', function () {
-            assert.equal(2, [1, 2, 3].indexOf(9));
+        it('should fail and pass ', function () {
+            assert.equal(1, [1, 2, 3].indexOf(2));
         });
     });
 });
